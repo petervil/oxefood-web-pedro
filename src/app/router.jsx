ebaf53e9db185435c867e0977f1b0d1f23d12a81
petervil <pedro.borba.vilela@gmail.com>
2026-09-21 21:14:51 -0300

    aula 21/09 - conteúdo 10

---
+++ src/app/router.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import ClientePage from "../features/cliente/page/ClientePage";
+import Home from "../features/home/page/Home";
+
+
+export default function Router() {
+
+    return (
+
+        <BrowserRouter>
+
+            <Routes>
+                <Route path="/home" element={<Home />} />
+
+                <Route path="/cliente" element={<ClientePage />} />
+            </Routes>
+
+        </BrowserRouter>
+
+    );
+}
